76411e20802d4529d3f9fb470dc352377d87c367
Pasha k <pavelpervi@hotmail.com>
2018-06-10 11:06:24 +0300

    Update svn_version_update.bat

--- svn_version_update.bat
+++ svn_version_update.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 :REM This script modify token $$Revision $$ in TARGET_FILE_NAME
-set TARGET_FILE_NAME=index.html
-set TARGET_FILE_PATH=%cd%\%TARGET_FILE_NAME%
+set TARGET_PATH_FILE_NAME=index.html
+set FULL_PATH=%cd%\%TARGET_PATH_FILE_NAME%
 
 for /f "delims=" %%a in ('svn info ^| findstr /C:"Revision"') do (
 	echo "Updating revision to -> %%a"
-	powershell -Command "(gc %TARGET_FILE_PATH%) -replace '\$\$SVN_Revision: [0-9]*\$\$', '$$$SVN_%%a$$$' | sc %TARGET_FILE_PATH%"
+	powershell -Command "(gc %FULL_PATH%) -replace '\$\$SVN_Revision: [0-9]*\$\$', '$$$SVN_%%a$$$' | sc %FULL_PATH%"
 )
