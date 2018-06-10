dca73cd61d53f8a7c63e990d9627c26e46bcae8d
Pasha k <pavelpervi@hotmail.com>
2018-06-10 11:25:51 +0300

    (*)Fixed: bug with spaces in path

--- svn_version_update.bat
+++ svn_version_update.bat
@@ -4,7 +4,7 @@ setlocal
 set TARGET_PATH_FILE_NAME=index.html
 set FULL_PATH=%cd%\%TARGET_PATH_FILE_NAME%
 
-for /f "delims=" %%a in ('svn info ^| findstr /C:"Revision"') do (
+for /f "delims=" %%a in ('svn info ^| findstr /R /C:"Revision[:][ ][0-9]*"') do (
 	echo "Updating revision to -> %%a"
-	powershell -Command "(gc %FULL_PATH%) -replace '\$\$SVN_Revision: [0-9]*\$\$', '$$$SVN_%%a$$$' | sc %FULL_PATH%"
+	powershell -Command "(gc '%FULL_PATH%') -replace '\$\$SVN_Revision:[ ]*[0-9]*\$\$', '$$$SVN_%%a$$$' | sc '%FULL_PATH%'"
 )
